e8b31b052bc5bb7c1e163ca2835315604114e6fa
AbdulteefRashid <abdulte97@gmail.com>
2025-06-27 02:12:21 +0300

    build login Screen ui

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,13 +1,91 @@
+import 'package:doctor_appotmnet/core/helper/spacing.dart';
+import 'package:doctor_appotmnet/core/theming/styles.dart';
+import 'package:doctor_appotmnet/core/utils/const/app_string.dart';
+import 'package:doctor_appotmnet/core/widgets/app_text_button.dart';
+import 'package:doctor_appotmnet/core/widgets/app_text_form_field.dart';
+import 'package:doctor_appotmnet/features/login/ui/widgets/dont_have_account_text.dart';
+import 'package:doctor_appotmnet/features/login/ui/widgets/terms_and_conditions_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+  bool isObscureText = true;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('LogIn Screen')),
-      body: Center(child: Text('LogIn Screen')),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  AppStrings.welcomeBackTitle,
+                  style: TextStyles.font24BlueBold,
+                ),
+                verticalSpace(8),
+                Text(
+                  AppStrings.weAreExitedTitle,
+                  style: TextStyles.font14GrayRegular,
+                ),
+                verticalSpace(36),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      AppTextFormField(hintText: AppStrings.emailLabelText),
+                      verticalSpace(18),
+                      AppTextFormField(
+                        hintText: AppStrings.passwordLabelText,
+                        isObscureText: isObscureText,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isObscureText = !isObscureText;
+                            });
+                          },
+                          child: Icon(
+                            isObscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                        ),
+                      ),
+                      verticalSpace(24),
+                      Align(
+                        alignment: AlignmentDirectional.centerEnd,
+                        child: Text(
+                          AppStrings.forgetPasswordTitle,
+                          style: TextStyles.font13BlueRegular,
+                        ),
+                      ),
+                      verticalSpace(40),
+                      AppTextButton(
+                        buttonText: AppStrings.loginTitle,
+                        textStyle: TextStyles.font16WhiteSemiBold,
+                        onPressed: () {},
+                      ),
+                      verticalSpace(16),
+                      const TermsAndConditionsText(),
+                      verticalSpace(16),
+                      const DontHaveAccountText(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
