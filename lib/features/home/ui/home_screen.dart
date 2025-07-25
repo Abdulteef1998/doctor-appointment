3643a8b82b4e733612696178ae73c9cad5a9e906
AbdulteefRashid <abdulte97@gmail.com>
2025-07-26 02:31:19 +0300

    filters doctors by id shimmer loading ui inhancmence for specailty image

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,15 +1,13 @@
 import 'package:doctor_appotmnet/core/helper/spacing.dart';
-import 'package:doctor_appotmnet/features/home/data/models/specializations_response_model.dart';
 import 'package:doctor_appotmnet/features/home/ui/widgets/doctors_blue_container.dart';
+import 'package:doctor_appotmnet/features/home/ui/widgets/doctors_list/doctors_bloc_builder.dart';
 import 'package:doctor_appotmnet/features/home/ui/widgets/doctors_speciality_see_all.dart';
 import 'package:doctor_appotmnet/features/home/ui/widgets/home_top_bar.dart';
-import 'package:doctor_appotmnet/features/home/ui/widgets/specializations_and_doctors_bloc_builder.dart';
+import 'package:doctor_appotmnet/features/home/ui/widgets/specializations_list/specializations_bloc_builder.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
-  final SpecializationsResponseModel? specializationsResponseModel;
-  const HomeScreen({super.key, this.specializationsResponseModel});
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,17 +16,18 @@ class HomeScreen extends StatelessWidget {
       body: SafeArea(
         child: Container(
           width: double.infinity,
-
-          margin: const EdgeInsets.fromLTRB(20, 16, 20, 20),
+          margin: const EdgeInsets.fromLTRB(20.0, 16.0, 20.0, 28.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const HomeTopBar(),
               const DoctorsBlueContainer(),
-              verticalSpace(24.h),
+              verticalSpace(24),
               const DoctorsSpecialitySeeAll(),
               verticalSpace(18),
-              SpecializationsAndDoctorsBlocBuilder(),
+              const SpecializationsBlocBuilder(),
+              verticalSpace(8),
+              const DoctorsBlocBuilder(),
             ],
           ),
         ),
